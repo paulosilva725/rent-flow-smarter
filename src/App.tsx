91e4b8dfc960368dd7b2ffaca625f32833323e14
paulosilva725 <djdjou2008@hotmail.com>
2025-07-28 22:42:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/RealDashboard";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import SystemDashboard from "./pages/SystemDashboard";
 import NotFound from "./pages/NotFound";
@@ -33,5 +32,4 @@ const App = () => (
     </TooltipProvider>
   </QueryClientProvider>
 );
-<SpeedInsights />
 export default App;
